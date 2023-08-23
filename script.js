49bfa818a4aa45e9a4806c2b928c61ef2383cd2d
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-23 21:33:09 +0700

    fix bug password clear after click submit

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const password = document.getElementById('password');
 const confirm = document.getElementById('password-confirm');
 
 cta.addEventListener('click', () => {
-    if(password.value === confirm.value) {
-        alert('Selamat!');
+    if(password.value == confirm.value) {
+        console.log('selamat')
+        password.classList.remove('error');
+        confirm.classList.remove('error');
+        const errorText = document.querySelector('.error-text');
+        if(errorText){
+            errorText.textContent = ''
+        }
+        password.value = ''
+        confirm.value = ''
+        return;
     }
     const el = document.createElement('p');
     const parentEl = password.parentNode;
@@ -13,4 +22,6 @@ cta.addEventListener('click', () => {
     para.classList.add('error-text');
     password.classList.toggle('error');
     confirm.classList.toggle('error');
+    password.value = ''
+    confirm.value = ''
 })
